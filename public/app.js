6b6a93a3ee769823aa43d1176560be69edb93ea5
Bergeronimo <5186239+Bergeronimo@users.noreply.github.com>
2023-09-06 08:30:59 -0500

    dots synced

--- public/app.js
+++ public/app.js
@@ -1,12 +1,21 @@
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('main-canvas');
+canvas.width = 640;
+canvas.height = 640;
 const ctx = canvas.getContext('2d');
 
+
+const SPEED = 1;
 let circles = {};
-let myCircle = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    radius: 20
-};
+let eatenFoodIDs = [];
+let foodDots = {};
+const foodDotColor = "#00FF00";
+const foodDotRadius = 2;
+
+// circles[socket.id] = {
+//     x: canvas.width / 2,
+//     y: canvas.height / 2,
+//     radius: 20
+// };
 
 const keysPressed = {
     ArrowUp: false,
@@ -15,37 +24,9 @@ const keysPressed = {
     ArrowRight: false
 };
 
-function drawCircle(circleData, id) {
-    ctx.beginPath();
-    ctx.arc(circleData.x, circleData.y, circleData.radius, 0, Math.PI * 2);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
 
-    // Adjust font, position, and color for the ID text
-    ctx.font = (id === socket.id) ? 'bold 12px Arial' : '12px Arial';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'left';
-    ctx.textBaseline = 'top';
-    ctx.fillText(id, circleData.x - circleData.radius, circleData.y - circleData.radius);
-}
 
-function update() {
-    const speed = 5;
 
-    if (keysPressed.ArrowUp) myCircle.y -= speed;
-    if (keysPressed.ArrowDown) myCircle.y += speed;
-    if (keysPressed.ArrowLeft) myCircle.x -= speed;
-    if (keysPressed.ArrowRight) myCircle.x += speed;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let id in circles) {
-        drawCircle(circles[id], id);
-    }
-
-    socket.emit('move circle', myCircle);
-    requestAnimationFrame(update);
-}
 
 const socket = io.connect('http://98.197.238.51:3000');
 
@@ -61,8 +42,26 @@ document.addEventListener('keyup', (event) => {
     }
 });
 
-socket.on('update circles', (data) => {
-    circles = data;
+socket.on('server update circles', (data) => {
+    if (socket.id in circles) {
+        data[socket.id] = circles[socket.id];
+        circles = data;
+    }
+    else {
+        circles = data;
+    }
+});
+
+socket.on('server consumed food dot ids', (allEatenFoodDotIds) => {
+    allEatenFoodDotIds.forEach((eatenFoodDotId) => {
+        if (foodDots.hasOwnProperty(eatenFoodDotId)) {
+            delete foodDots[eatenFoodDotId];
+        }
+    });
+});
+
+socket.on('server set all food dots', (data) => {
+    foodDots = data;
 });
 
 let pingTimestamp;
@@ -70,15 +69,132 @@ let pingTimestamp;
 // Emit ping event to the server
 function sendPing() {
     pingTimestamp = Date.now();
-    socket.emit('ping', pingTimestamp);
+    socket.emit('client ping', pingTimestamp);
 }
 
 // Listen for pong event from the server
-socket.on('pong', (timestamp) => {
+socket.on('server pong', (timestamp) => {
     const latency = Date.now() - timestamp;
     document.getElementById('pingDisplay').textContent = `Ping: ${latency}ms`;
 });
-
 setInterval(sendPing, 1000);
+
+// listen for a new food dot event
+socket.on('server new food dot kv', (foodDotKV) => {
+    let key = foodDotKV["key"];
+    let foodDot = foodDotKV["value"];
+    foodDots[key] = foodDot;
+});
+
+// transmit updated circle positions
+setInterval(() => {
+    const myCircle = circles[socket.id];
+    socket.emit('client player update', myCircle);
+}, 1000 / 15);
+
+// transmit consumed food dots
+setInterval(() => {
+    socket.emit('client consumed food dots', eatenFoodIDs);
+    eatenFoodIDs = [];
+}, 1000 / 8);
+
+function drawCircle(circleData, id) {
+    ctx.beginPath();
+    ctx.arc(circleData.x, circleData.y, circleData.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+
+    // Draw a red bounding box around the circle
+    ctx.beginPath();
+    ctx.rect(
+        circleData.x - circleData.radius, // X coordinate of the top-left corner
+        circleData.y - circleData.radius, // Y coordinate of the top-left corner
+        circleData.radius * 2,           // Width of the rectangle
+        circleData.radius * 2            // Height of the rectangle
+    );
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2; // Adjust the line width as needed
+    ctx.stroke();
+    ctx.closePath();
+
+    // Adjust font, position, and color for the ID text
+    ctx.font = (id === socket.id) ? 'bold 12px Arial' : '12px Arial';
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText(id, circleData.x - circleData.radius, circleData.y - circleData.radius);
+}
+
+function drawFoodDot(foodDot, id) {
+    ctx.beginPath();
+    ctx.arc(foodDot.x, foodDot.y, foodDotRadius, 0, Math.PI * 2);
+    ctx.fillStyle = foodDotColor;
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+function update() {
+    if (socket.id) {
+        const myCircle = circles[socket.id];
+        if (myCircle) {
+            // process input
+            myCircle.vx = 0;
+            myCircle.vy = 0;
+            if (keysPressed.ArrowUp) myCircle.vy = -SPEED;
+            if (keysPressed.ArrowDown) myCircle.vy = SPEED;
+            if (keysPressed.ArrowLeft) myCircle.vx = -SPEED;
+            if (keysPressed.ArrowRight) myCircle.vx = SPEED;
+        }
+    }
+
+
+    // step all circles
+    Object.values(circles).forEach(circle => {
+        if (circle) {
+            circle.x += circle.vx;
+            circle.y += circle.vy;
+        }
+    });
+
+    // check if any foods are eaten
+
+    const circle = circles[socket.id];
+    if (circle) {
+        for (const foodID in foodDots) {
+            if (foodDots.hasOwnProperty(foodID)) {
+                const foodDot = foodDots[foodID];
+
+                // Calculate the distance between the circle and the food dot
+                const distance = Math.sqrt(
+                    Math.pow(circle.x - foodDot.x, 2) + Math.pow(circle.y - foodDot.y, 2)
+                );
+
+                if (distance < circle.radius) {
+                    circle.radius += 1;
+                    eatenFoodIDs.push(foodID);
+                    delete foodDots[foodID];
+                }
+            }
+        }
+    }
+
+    // DRAW
+    // draw players
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let id in circles) {
+        let circle = circles[id];
+        if (circle) {
+            drawCircle(circle, id);
+        }
+    }
+    // draw food dots
+    for (let id in foodDots) {
+        drawFoodDot(foodDots[id], id);
+    }
+
+    requestAnimationFrame(update);
+}
 
 update();
--- server.js
+++ server.js
@@ -7,7 +7,41 @@ app.use(express.static('public'));
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const WORLD_WIDTH = 640;
+const WORLD_HEIGHT = 640;
+
 let circles = {}; // Store circle data for each client
+
+// FOOD DOTS
+let nextFoodDotId = 0;
+let foodDots = {};
+let allEatenFoodDotIds = [];
+const maxFoodDots = 32;
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function trySpawnRandomFoodDot() {
+    const keys = Object.keys(foodDots);
+    if (keys.length >= maxFoodDots) {
+        return;
+    }
+    const x = getRandom(0, WORLD_WIDTH);
+    const y = getRandom(0, WORLD_HEIGHT);
+
+    const foodDotKey = nextFoodDotId;
+    nextFoodDotId += 1;
+
+    let newFoodDot = { x, y }
+    foodDots[foodDotKey] = newFoodDot;
+
+    let foodDotKV = { "key": foodDotKey, "value": newFoodDot }
+    return foodDotKV;
+}
+
+// for (let i = 0; i < maxFoodDots; i++) {
+//     trySpawnRandomFoodDot()
+// }
+
 
 io.on('connection', (socket) => {
     console.log('a user connected:', socket.id);
@@ -16,27 +50,57 @@ io.on('connection', (socket) => {
     circles[socket.id] = {
         x: Math.random() * 500, // Random position for demonstration
         y: Math.random() * 500,
-        radius: 20
+        vx: 0,
+        vy: 0,
+        radius: 4
     };
 
     // Send the updated circles data to all clients
-    io.emit('update circles', circles);
+    socket.emit('update circles', circles);
+    socket.emit('server set all food dots', foodDots);
 
-    socket.on('move circle', (data) => {
+    socket.on('client player update', (data) => {
         circles[socket.id] = data; // Update the circle data for this client
-        io.emit('update circles', circles); // Broadcast updated data
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected:', socket.id);
         delete circles[socket.id]; // Remove circle data for the disconnected client
-        io.emit('update circles', circles); // Broadcast updated data
+        io.emit('server update circles', circles); // Broadcast updated data
     });
 
-    socket.on('ping', (timestamp) => {
-        socket.emit('pong', timestamp);
+    socket.on('client ping', (timestamp) => {
+        socket.emit('server pong', timestamp);
     });
+
+    socket.on('client consumed food dots', (eatenFoodDotIds) => {
+        eatenFoodDotIds.forEach((eatenFoodDotId) => {
+            if (foodDots.hasOwnProperty(eatenFoodDotId)) {
+                delete foodDots[eatenFoodDotId];
+            }
+        });
+        allEatenFoodDotIds = allEatenFoodDotIds.concat(eatenFoodDotIds);
+    });
+
+    // update all circles 
+    setInterval(() => {
+        io.emit('server update circles', circles); // Broadcast updated data
+    }, 1000 / 15);
+
+    // transmit all consumed food dots 
+    setInterval(() => {
+        io.emit('server consumed food dot ids', allEatenFoodDotIds);
+        allEatenFoodDotIds = [];
+    }, 1000 / 8);
 });
+
+
+// periodically spawn food dots
+setInterval(() => {
+    let foodDotKV = trySpawnRandomFoodDot();
+    io.emit('server new food dot kv', foodDotKV);
+
+}, 1000 / 1);
 
 server.listen(3000, '0.0.0.0', () => {
     console.log('listening on *:3000');
